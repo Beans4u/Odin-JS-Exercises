2ce4d62fc42dff153e68510b01b9986a83725fd1
Carina Garrity <carinamgarrity@gmail.com>
2021-01-27 12:35:48 -0700

    complete Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
+const palindromes = function(x) {
+    
+    
+    let replace = /[\W]/g;
 
+    let lcString = x.toLowerCase().replace(replace, "");
+    
+    let revString = lcString.split('').reverse().join('');
+
+    return lcString == revString;
+    
 }
 
 module.exports = palindromes
