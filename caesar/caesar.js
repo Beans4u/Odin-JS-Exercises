1deb4868dce6e1f74137af1158f10b38b78850b7
Carina Garrity <carinamgarrity@gmail.com>
2021-02-04 13:25:27 -0700

    solve fibonacci

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,31 @@
-const caesar = function() {
+const caesar = function(str, num) {
+            num = num % 26;
+            
+            var lowerCaseStr = str.toLowerCase();
+            var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+            var newString = '';
+        
+            for(var i = 0; i < lowerCaseStr.length; i++){
+                var currentLetter = lowerCaseStr[i];
+                if(currentLetter !== alphabet || currentLetter !== alphabet.toUpperCase()){ // change to !== alphabet later
+                    newString += currentLetter;
+                    continue;
+                }
+                var currentIndex = alphabet.indexOf(currentLetter);
+                var newIndex = currentIndex + num;
+        
+                if(newIndex > 25) {
+                    newIndex = newIndex - 26;
+                }
+                if(newIndex < 0) {
+                    newIndex = newIndex + 26;
+                }
+                if(str[i] === str[i].toUpperCase()) {
+                    newString += alphabet[newIndex].toUpperCase();
+                } else newString += alphabet[newIndex];
+            }
+            return newString;
+        }
 
-}
 
 module.exports = caesar
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [];
 
+    fib[0] = 0;
+    fib[1] = 1;
+    
+    for (let i = 2; i <= 25; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    } 
+       
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        return fib[num];
+    }
 }
 
 module.exports = fibonacci
