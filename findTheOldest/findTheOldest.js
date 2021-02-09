9c1cbdc4c1dfc2da8500112d93d392fcc6a63777
Carina Garrity <carinamgarrity@gmail.com>
2021-02-09 13:17:35 -0700

    solve findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-let findTheOldest = function() {
+const today = new Date();
+
+function findAge(person) {
+    const endYear = person.yearOfDeath || today.getFullYear();
+    return endYear - person.yearOfBirth;
+}
+
+let findTheOldest = function(people) {
+
+    const sortedByAge = people.sort((personA, personB)=>{
+        return findAge(personB) - findAge(personA);
+      });
+  
+      return sortedByAge[0];
 
 }
 
